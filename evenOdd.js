90b00d9e1a4331a23ed2b6fec444a7e2f26d7ec0
Rownak Abdullah <rownakabdullahomi@gmail.com>
2025-02-16 18:15:48 +0600

    problem 1 firnd even odd done

---
+++ evenOdd.js
@@ -0,0 +1,15 @@
+function evenOrOdd(n) {
+    if (n < 0) {
+        console.log("Number is negative");
+    } else if (n % 2 === 0) {
+        console.log("Even");
+    } else {
+        console.log("Odd");
+    }
+}
+
+// evenOrOdd(4);
+// evenOrOdd(7);
+// evenOrOdd(-3);
+
+
